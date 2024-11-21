4885afbf2553f19f869c4429a2c4e15dce82ae99
Julio <148032904+juliobrito7@users.noreply.github.com>
2024-11-20 21:20:18 -0300

    domain updated

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://book-cloud-e-library.vercel.app',
     methods: ['GET', 'POST'],
     credentials: true,
 }));
