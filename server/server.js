7914a5f457444d7a9d113c66535e527daec98e63
Julio <148032904+juliobrito7@users.noreply.github.com>
2024-11-21 16:40:27 -0300

    changes on server.js done

--- server/server.js
+++ server/server.js
@@ -48,10 +48,10 @@ app.use("/api/payments", paymentRoutes);
 app.use("/api/analytics", analyticsRoutes);
 
 if (isProduction) {
-    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+    app.use(express.static(path.join(__dirname, "/client/dist")));
 
     app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+        res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
     });
 }
 
